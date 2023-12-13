f28f77fe33c7a0e13e5ff58a1af094ccb53c6023
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-12 19:46:22 -0800

    Add unferlines to links

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,11 +1,21 @@
 export default function Footer() {
   return (
-    <footer className="flex justify-between p-2">
+    <footer className="flex justify-between px-4 py-2">
       <span>
-        <a href="https://github.com/CastillejaCode">Julian</a> and{" "}
-        <a href="https://github.com/coderaries12">Sheema</a>
+        <a href="https://github.com/CastillejaCode" className="underline">
+          Julian
+        </a>{" "}
+        and{" "}
+        <a href="https://github.com/coderaries12" className="underline">
+          Sheema
+        </a>
       </span>
-      <a href="https://github.com/coderaries12/Guess-Game">github</a>
+      <a
+        href="https://github.com/coderaries12/Guess-Game"
+        className="underline"
+      >
+        github
+      </a>
     </footer>
   );
 }
