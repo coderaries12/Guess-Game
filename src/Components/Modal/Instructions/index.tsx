7a7c928d686c064a8fe64e6b9b52744f57a4df68
Sheena <aries.sheen@gmail.com>
2023-12-12 17:03:26 -0800

    Instruction file

---
+++ src/Components/Modal/Instructions/index.tsx
@@ -0,0 +1,9 @@
+
+export default function Instructions(){
+    return(
+        <>
+        <p>The three buzz words are given </p>
+        <p>You have to guess the right word</p>
+        </>
+    )
+}
