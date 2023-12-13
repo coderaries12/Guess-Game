9af139663246763acaaf8596f3de5b479e727969
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-13 07:12:41 -0800

    Remove animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      animation: {
-        shake: "shake .87s cubic-bezier(.36,.07,.19,.97) both",
-      },
-      keyframes: {
-        shake: {
-          "10%, 90%": { transform: "translate3d(-2px,0,0)" },
-          "20%, 80%": { transform: "translate3d(4px,0,0)" },
-          "30%,50%, 70%": { transform: "translate3d(-8px,0,0)" },
-          "40%, 60%": { transform: "translate3d(8px,0,0)" },
-        },
-      },
-    },
+    extend: {},
   },
 
   plugins: [require("daisyui")],
