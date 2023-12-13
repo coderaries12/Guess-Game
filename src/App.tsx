8b8ce597034f8247f8fae1de6497b214aafc9ace
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-12 19:34:51 -0800

    Move text to new compoenent

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,17 @@
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
+import TextAnimated from "./Components/TextAnimated";
 import Game from "./routes/game";
 import Landing from "./routes/landing";
 import { Route } from "wouter";
-import { motion } from "framer-motion";
 
 const title = "guess";
-const list = {
-  visible: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      delay: 0.3,
-      when: "beforeChildren",
-      staggerChildren: 0.1,
-    },
-  },
-  hidden: {
-    opacity: 0,
-    y: -200,
-  },
-};
-const item = {
-  visible: { opacity: 1, y: 0 },
-  hidden: { opacity: 0, y: -200 },
-};
 
 function App() {
   return (
     <div className="relative flex min-h-screen flex-col">
       <Header title={title} />
-      <motion.div
-        variants={list}
-        initial="hidden"
-        animate="visible"
-        className="absolute top-28 w-fit self-center text-center font-['Berkshire_Swash']  text-7xl leading-normal text-red-900"
-      >
-        <motion.h1 variants={item}>Christmas</motion.h1>
-        <motion.h1 variants={item}>Edition</motion.h1>
-      </motion.div>
+      <Route path="/" component={TextAnimated} />
       <main className="flex flex-1 flex-col items-center justify-center">
         <Route path="/">
           <Landing />
