128ed0663cde7e63775edec6cde014d1a9855cb0
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-12 18:19:30 -0800

    Add subheading animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,32 @@ import Header from "./Components/Header";
 import Game from "./routes/game";
 import Landing from "./routes/landing";
 import { Route } from "wouter";
+import { motion } from "framer-motion";
 
 const title = "guess";
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="relative flex min-h-screen flex-col">
       <Header title={title} />
+      <motion.div className="my-12 flex flex-col gap-2">
+        <motion.h2
+          initial={{ y: -250 }}
+          animate={{ y: 0 }}
+          transition={{ type: "spring", delay: 2 }}
+          className=" text-center text-6xl"
+        >
+          Christmas
+        </motion.h2>
+        <motion.h2
+          initial={{ y: -250 }}
+          animate={{ y: 0 }}
+          transition={{ type: "spring", delay: 2.5 }}
+          className="text-center text-6xl"
+        >
+          Edition
+        </motion.h2>
+      </motion.div>
       <main className="flex flex-1 flex-col items-center justify-center">
         <Route path="/">
           <Landing />
