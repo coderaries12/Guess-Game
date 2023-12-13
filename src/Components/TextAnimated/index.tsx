6ddbb004a5267f7bd5a5e8740bbc08ebe0523fd5
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-13 07:20:30 -0800

    Style landing page for mobile

--- src/Components/TextAnimated/index.tsx
+++ src/Components/TextAnimated/index.tsx
@@ -1,15 +1,14 @@
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 
 const list = {
   visible: {
     opacity: 1,
     y: 0,
-    
+
     transition: {
       delay: 0.3,
       when: "beforeChildren",
       staggerChildren: 0.1,
-      
     },
   },
   hidden: {
@@ -22,17 +21,18 @@ const item = {
   hidden: { opacity: 0, y: -200 },
 };
 
-export default function TextAnimated () {
-    return(
-        
-        <motion.div
-        variants={list}
-        initial="hidden"
-        animate="visible"
-        className="absolute top-28 w-fit self-center text-center font-['Berkshire_Swash']  text-4xl leading-normal text-orange-700"
->
-        <motion.h1 variants={item}>Test your Christmas knowledge and find the hidden word!</motion.h1>
-        {/* <motion.h1 variants={item}>Edition</motion.h1> */}
-      </motion.div>
-    )
+export default function TextAnimated() {
+  return (
+    <motion.div
+      variants={list}
+      initial="hidden"
+      animate="visible"
+      className="w-fit self-center px-4 text-center font-['Berkshire_Swash']  text-4xl leading-normal text-orange-700"
+    >
+      <motion.h1 variants={item}>
+        Test your Christmas knowledge and find the hidden word!
+      </motion.h1>
+      {/* <motion.h1 variants={item}>Edition</motion.h1> */}
+    </motion.div>
+  );
 }
