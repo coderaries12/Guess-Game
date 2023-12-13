8348de3405069d9cae643ca2593a0772beaa4e4a
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-12 17:49:21 -0800

    Add plaster font

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -5,7 +5,7 @@ import { Link } from "wouter";
 
 export default function Header({ title }: Props) {
   return (
-    <header className="flex justify-center p-4 text-4xl">
+    <header className="flex justify-center p-4 font-[Plaster] text-4xl">
       <Link to="/">{title}</Link>
     </header>
   );
