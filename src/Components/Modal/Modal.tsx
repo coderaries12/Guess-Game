ff10167b51c49d2f826c68785c6b706b8eae7c5a
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-12 16:46:07 -0800

    Add exit button

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -12,8 +12,16 @@ function Modal({ children }: Props) {
       <button className="btn" onClick={() => modalRef.current?.showModal()}>
         instructions
       </button>
-      <dialog id="my_modal_2" className="modal" ref={modalRef}>
-        <div className="modal-box">{children}</div>
+      <dialog className="modal" ref={modalRef}>
+        <div className="modal-box p-4">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          {children}
+        </div>
         <form method="dialog" className="modal-backdrop">
           <button>close</button>
         </form>
