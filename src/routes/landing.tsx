86700472a29079e8ea64d635a6c03a9a363bb9f7
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-12 17:24:04 -0800

    Refactor to separate component

--- src/routes/landing.tsx
+++ src/routes/landing.tsx
@@ -1,12 +1,11 @@
+import Header from "../Components/Header";
 import { Modal } from "../Components/Modal";
 import { Link } from "wouter";
 
 export default function Landing() {
   return (
     <div className="flex min-h-screen flex-col justify-between">
-      <header className="flex justify-center p-4 text-4xl">
-        <h1>Guess</h1>
-      </header>
+      <Header title={"guess"} />
       <main className="flex flex-1 flex-col items-center justify-center">
         <div className="flex flex-col gap-4">
           <Link to="/game">Play</Link>
