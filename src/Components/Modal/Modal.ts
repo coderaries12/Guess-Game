2ed7cc4faa940ad928da99933e45582784c4d83f
Sheena <aries.sheen@gmail.com>
2023-12-12 16:02:01 -0800

    Created modal function

--- src/Components/Modal/Modal.ts
+++ src/Components/Modal/Modal.ts
@@ -3,34 +3,20 @@
 // Import any necessary dependencies or modal-related modules
 import './Modal.css'
 
-class Modal {
-    private modalElement: HTMLElement;
-    private closeButton: HTMLElement;
-  
-    constructor(modalId: string) {
-      // Assuming you have an HTML structure with a modal element and a close button
-      this.modalElement = document.getElementById(modalId) as HTMLElement;
-      this.closeButton = this.modalElement.querySelector('.close-button') as HTMLElement;
-  
-      // Add event listeners
-      this.closeButton.addEventListener('click', this.closeModal.bind(this));
-      document.addEventListener('keydown', this.handleKeyDown.bind(this));
-    }
-  
-    private closeModal() {
-      this.modalElement.style.display = 'none';
-    }
-  
-    private openModal() {
-      this.modalElement.style.display = 'block';
-    }
-  
-    private handleKeyDown(event: KeyboardEvent) {
-      if (event.key === 'Escape') {
-        this.closeModal();
-      }
-    }
+function showModal(modalId: string): void {
+  const modalElement = document.getElementById(modalId);
+
+  if (modalElement) {
+    modalElement.style.display = 'block';
   }
-  
-  export default Modal;
-  
+}
+
+function hideModal(modalId: string): void {
+  const modalElement = document.getElementById(modalId);
+
+  if (modalElement) {
+    modalElement.style.display = 'none';
+  }
+}
+
+export { showModal, hideModal };
