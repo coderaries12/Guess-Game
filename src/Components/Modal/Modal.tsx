c77302dcb356b57020c2d19b662587c279731299
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-12-12 16:41:27 -0800

    Add styled modal

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,18 +1,25 @@
-import { useRef } from 'react';
-import './Modal.css';
+import { useRef } from "react";
+import "./Modal.css";
 
 interface Props {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 function Modal({ children }: Props) {
-	const modalRef = useRef<HTMLDialogElement>(null);
-	return (
-		<>
-			<button onClick={() => modalRef.current?.showModal()}>{children}</button>
-			<dialog ref={modalRef}>{children}</dialog>
-		</>
-	);
+  const modalRef = useRef<HTMLDialogElement>(null);
+  return (
+    <>
+      <button className="btn" onClick={() => modalRef.current?.showModal()}>
+        instructions
+      </button>
+      <dialog id="my_modal_2" className="modal" ref={modalRef}>
+        <div className="modal-box">{children}</div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
+  );
 }
 
 export { Modal };
